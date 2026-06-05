420e4a6577ed9fadc909e49497b22137f0aea7cd
amarnathrp1907 <amarnathrp1907@gmail.com>
2026-06-05 18:59:14 +0000

    Coversion_of_datatypes

---
+++ 01_basics/03_datatypeconversions.js
@@ -0,0 +1,20 @@
+let score="33"
+let valueofScore=Number(score)
+console.log(typeof valueofScore)
+console.log(valueofScore);
+
+// "33"=> 33
+// "33abc"=>Nan,undefined=>Nan
+// null=>0
+// true=>1,false=>0
+
+let isLoggedIn=1
+let boolean_of=Boolean(isLoggedIn)
+console.log(boolean_of);
+
+// 1=> true,0=>false
+// ""=>false,"Amarnath"=>true
+
+let abc=77
+let string_val=String(abc)
+console.table([string_val,typeof string_val]);
