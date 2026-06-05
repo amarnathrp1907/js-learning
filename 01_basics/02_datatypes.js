0ddc0285e6a4e1bdd021f34a0b51b776270bb42c
amarnathrp1907 <amarnathrp1907@gmail.com>
2026-06-05 15:25:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all js code as newer version
+// alert(3+3) // we are using nodejs not browser
+
+// code readability should be high
+
+let name="Amarnath"
+let age=20
+let isLoggedIn=true
+
+// number => 2 to power 53
+// bigInt
+// string ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned yet
+// symbol => unique 
+
+console.log(typeof("Amarnath"))
+console.log(typeof(age))
+console.log(typeof(isLoggedIn))
+console.log(typeof(null)) // type is object 
+console.log(typeof(undefined)) // type is undefined 
