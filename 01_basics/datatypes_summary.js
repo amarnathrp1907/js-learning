30e86290684ecca2b42b2b272ead2ba3ea73707c
amarnathrp1907 <amarnathrp1907@gmail.com>
2026-06-06 13:41:27 +0000

    summary of datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,8 @@
+// Primitive 
+// 7 types : String , Number , Bollean, Symbol , Null , Undefined , BigInt
+let abc=Symbol(2)
+let xyz=Symbol(3)
+console.log(abc==xyz) // false
+
+// Reference (Non Primtive)
+// Functions,Objects,Array
